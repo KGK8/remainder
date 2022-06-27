61fa3c49f5e89f6b58801a958bf998ef825e8756
Giridhar Kolavasi <giridhar.kolavasi@cloudfuze.com>
2022-06-28 03:31:11 +0530

    prod api 4

--- src/App.js
+++ src/App.js
@@ -211,6 +211,16 @@ const App = () => {
               <option>12</option>
               <option>13</option>
               <option>14</option>
+              <option>15</option>
+              <option>16</option>
+              <option>17</option>
+              <option>18</option>
+              <option>19</option>
+              <option>20</option>
+              <option>21</option>
+              <option>22</option>
+              <option>23</option>
+              <option>24</option>
             </Input>
             <Input
               type="select"
